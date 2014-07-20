fff17fcc456b14e7b327f39e255cc56627bd3e85
gcamp <guillaume.campagna@gmail.com>
2014-07-20 17:48:21 -0400

    Create github_helper module

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function configValueForKey(key, callback) {
     });
 }
 
+var github_helper = require('./github_helper');
 configValueForKey("user", function (user) {
   configValueForKey("password", function (password) {
     var GitHubApi = require("github");
@@ -26,32 +27,8 @@ configValueForKey("user", function (user) {
     github.issues.getAll({
       filter: "all"
     }, function(err, res) {
-      var ownerMap = {};
-
-      for (index in res) {
-        var issue = res[index];
-        var repo = issue["repository"];
-
-        if (repo) {
-          var repoName = repo["name"];
-          var owner = repo["owner"]["login"];
-
-          var projectsMap = ownerMap[owner];
-          if (!projectsMap) {
-            projectsMap = {};
-          }
-
-          var issues = projectsMap[repoName];
-          if (!issues) {
-            issues = [];
-          }
-          issues.push(issue["title"]);
-          projectsMap[repoName] = issues;
-
-          ownerMap[owner] = projectsMap;
-        }
-      }
-
+      var ownerMap = github_helper.group_issues(res);
+      
       console.log(JSON.stringify(ownerMap));
     });
   });
